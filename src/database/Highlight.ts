a7d6a9b99dbcb5d627aeb276e73fb85bfdc16069
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-08 17:47:42 -0700

    Init import all ability

--- src/database/Highlight.ts
+++ src/database/Highlight.ts
@@ -222,4 +222,19 @@ export class HighlightService {
 
         return originalContent
     }
+
+    async getAllBooks(): Promise<Map<string, BookDetails>> {
+        const books = await this.repo.getAllBookDetails();
+        const bookMap = new Map<string, BookDetails>();
+        
+        for (const book of books) {
+            bookMap.set(book.title, book);
+        }
+        
+        return bookMap;
+    }
+
+    async getAllContentByBookTitle(bookTitle: string): Promise<Content[]> {
+        return this.repo.getAllContentByBookTitle(bookTitle);
+    }
 }
